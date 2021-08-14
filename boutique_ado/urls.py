fee571983738be206bac853c7c27a9deea74f020
roomacarthur <roomacarthur@gmail.com>
2021-08-14 19:26:35 +0100

    added products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')), # include all urls from within the home app. 
+    path('products/', include('products.urls')), # include all urls from within the products app. 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
